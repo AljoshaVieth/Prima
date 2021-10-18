3eb1fdf5006651fad455081b0d561cdf91a7fed7
Aljosha <aljoshajustin@gmail.com>
2021-10-18 18:24:40 +0200

    ll: added simple rotation

--- projects/LaserLeague/Script/Source/Main.ts
+++ projects/LaserLeague/Script/Source/Main.ts
@@ -5,21 +5,23 @@ namespace Script {
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
+  let laserTransformMatrix: ƒ.Matrix4x4;
+
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     let graph: ƒ.Node = viewport.getBranch();
-    let laser: ƒ.Node = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser")[0];
-    transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
+    let laser: ƒ.Node = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser 1")[0];
+    laserTransformMatrix = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
     console.log(graph);
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.world.simulate();  // if physics is included and used
   
-    transform.rotateZ(5);
+    laserTransformMatrix.rotateZ(5);
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
