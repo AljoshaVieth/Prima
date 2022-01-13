bd1a37dec92c863c7b06e4048fda14e2b557522b
Aljosha <mail@aljoshavieth.de>
2022-01-13 14:50:57 +0100

    Y small changes

--- projects/TheJourneyOfY/Script/Build/Script.js
+++ projects/TheJourneyOfY/Script/Build/Script.js
@@ -1,1 +1,100 @@
-console.warn("%cFor scripting, start your favorite IDE (e.g. VSCode) with typescript installed, load and compile this project", "color: red; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        // Register the script as component for use in the editor via drag&drop
+        static iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+        // Properties may be mutated by users in the editor via the automatically created user interface
+        message = "CustomComponentScript added to ";
+        constructor() {
+            super();
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* NODE_DESERIALIZED */, this.hndEvent);
+        }
+        // Activate the functions of this component as response to events
+        hndEvent = (_event) => {
+            switch (_event.type) {
+                case "componentAdd" /* COMPONENT_ADD */:
+                    ƒ.Debug.log(this.message, this.node);
+                    break;
+                case "componentRemove" /* COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                    break;
+                case "nodeDeserialized" /* NODE_DESERIALIZED */:
+                    // if deserialized the node is now fully reconstructed and access to all its components and children is possible
+                    break;
+            }
+        };
+    }
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Debug.info("Main Program Template running!");
+    window.addEventListener("load", setup);
+    let viewport;
+    document.addEventListener("interactiveViewportStarted", start);
+    let graph;
+    let desiredZoomLevel = -30;
+    let currentZoomLevel = -80;
+    let cmpCamera;
+    let graphId = "Graph|2022-01-08T12:51:22.101Z|15244";
+    function start(_event) {
+        viewport = _event.detail;
+        graph = viewport.getBranch();
+        //graph.getComponents(ƒ.ComponentAudio)[1].play(true);
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    }
+    function update(_event) {
+        // ƒ.Physics.world.simulate();  // if physics is included and used
+        viewport.draw();
+        //zoom in
+        ƒ.Debug.info("update loop");
+        if (currentZoomLevel < desiredZoomLevel) {
+            currentZoomLevel++;
+            cmpCamera.mtxPivot.translateZ(1);
+        }
+        ƒ.AudioManager.default.update();
+    }
+    async function setup() {
+        console.log("setting up...");
+        await FudgeCore.Project.loadResourcesFromHTML();
+        let graph = ƒ.Project.resources[graphId];
+        // setup the viewport
+        cmpCamera = new FudgeCore.ComponentCamera();
+        cmpCamera.mtxPivot.rotateY(180);
+        cmpCamera.mtxPivot.translateZ(currentZoomLevel);
+        let canvas = document.querySelector("canvas");
+        graph.addComponent(cmpCamera);
+        let viewport = new FudgeCore.Viewport();
+        viewport.initialize("Viewport", graph, cmpCamera, canvas);
+        FudgeCore.Debug.log("Viewport:", viewport);
+        // hide the cursor when interacting, also suppressing right-click menu
+        //canvas.addEventListener("mousedown", canvas.requestPointerLock);
+        //canvas.addEventListener("mouseup", function () { document.exitPointerLock(); });
+        // make the camera interactive (complex method in FudgeAid)
+        //FudgeAid.Viewport.expandCameraToInteractiveOrbit(viewport);
+        // setup audio
+        /*
+        let cmpListener = new ƒ.ComponentAudioListener();
+        cmpCamera.node.addComponent(cmpListener);
+        FudgeCore.AudioManager.default.listenWith(cmpListener);
+        FudgeCore.AudioManager.default.listenTo(graph);
+        FudgeCore.Debug.log("Audio:", FudgeCore.AudioManager.default);
+         */
+        // draw viewport once for immediate feedback
+        viewport.draw();
+        canvas.dispatchEvent(new CustomEvent("interactiveViewportStarted", { bubbles: true, detail: viewport }));
+    }
+})(Script || (Script = {}));
+//# sourceMappingURL=Script.js.map
--- projects/TheJourneyOfY/Script/Source/Main.ts
+++ projects/TheJourneyOfY/Script/Source/Main.ts
@@ -2,19 +2,78 @@ namespace Script {
   import ƒ = FudgeCore;
   ƒ.Debug.info("Main Program Template running!");
 
+  window.addEventListener("load", setup);
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
+  let graph: ƒ.Node;
+  let desiredZoomLevel: number = -30;
+  let currentZoomLevel: number = -80;
+  let cmpCamera: any;
+  let graphId: string = "Graph|2022-01-08T12:51:22.101Z|15244";
+
+
+
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    graph = viewport.getBranch();
+
+
+    //graph.getComponents(ƒ.ComponentAudio)[1].play(true);
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.world.simulate();  // if physics is included and used
     viewport.draw();
+
+    //zoom in
+    ƒ.Debug.info("update loop");
+
+    if(currentZoomLevel < desiredZoomLevel) {
+      currentZoomLevel++;
+      cmpCamera.mtxPivot.translateZ(1);
+    }
+
     ƒ.AudioManager.default.update();
+  }
+
+
+
+  async function setup(): Promise<void> {
+    console.log("setting up...");
+    await FudgeCore.Project.loadResourcesFromHTML();
+    let graph: any = ƒ.Project.resources[graphId];
+    // setup the viewport
+    cmpCamera = new FudgeCore.ComponentCamera();
+    cmpCamera.mtxPivot.rotateY(180);
+    cmpCamera.mtxPivot.translateZ(currentZoomLevel);
+
+
+    let canvas = document.querySelector("canvas");
+    graph.addComponent(cmpCamera);
+    let viewport = new FudgeCore.Viewport();
+    viewport.initialize("Viewport", graph, cmpCamera, canvas);
+    FudgeCore.Debug.log("Viewport:", viewport);
+    // hide the cursor when interacting, also suppressing right-click menu
+    //canvas.addEventListener("mousedown", canvas.requestPointerLock);
+    //canvas.addEventListener("mouseup", function () { document.exitPointerLock(); });
+    // make the camera interactive (complex method in FudgeAid)
+    //FudgeAid.Viewport.expandCameraToInteractiveOrbit(viewport);
+
+    // setup audio
+    /*
+    let cmpListener = new ƒ.ComponentAudioListener();
+    cmpCamera.node.addComponent(cmpListener);
+    FudgeCore.AudioManager.default.listenWith(cmpListener);
+    FudgeCore.AudioManager.default.listenTo(graph);
+    FudgeCore.Debug.log("Audio:", FudgeCore.AudioManager.default);
+     */
+
+    // draw viewport once for immediate feedback
+    viewport.draw();
+    canvas.dispatchEvent(new CustomEvent("interactiveViewportStarted", { bubbles: true, detail: viewport }));
   }
 }
